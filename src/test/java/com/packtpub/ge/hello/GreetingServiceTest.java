70f0129b7ebeb29e2186b021699f687be903bf4c
Richard Jackson <rjackson@othsecurity.com>
2020-08-17 19:33:19 +0000

    Add testing code

---
+++ src/test/java/com/packtpub/ge/hello/GreetingServiceTest.java
@@ -0,0 +1,20 @@
+package com.packtpub.ge.hello;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class GreetingServiceTest {
+	GreetingService service;
+
+	@Before
+	public void setup(){
+		service = new GreetingService();
+	}
+
+	@Test
+	public void testGreet(){
+		// assertEquals("Hello Test", service.greet("Test"));
+		assertEquals("Hello Test", service.greet("Guest"));
+	}
+}
